f5906fc7dd19ef2577c83862ec5dd6bdecb61582
Paulo Marinho <pakas_vieira_95@hotmail.com>
2024-06-26 14:07:07 -0300

    chore: improve names

--- src/tunnel-server.js
+++ src/tunnel-server.js
@@ -6,6 +6,20 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+/**
+ *  MOTIVAÇÕES:
+ *      - Subir uma aplicação local alugando uma máquina muito barata da cloud (apenas para fazer o proxy);
+ *      - Possibilidade de um mesmo ip-fixo fazer o proxy pra vários ips dinâmicos (duckdns suport isso?);
+ *      - Diferente dos serviçoes padrões de ddns, vc pode configurar para rodar em seu próprio domínio,
+ *        pode ser útil para testar um MVP, ou para mandar pro ar todo um portifólio antes daquela entrevista;
+ *      - Vc está dos dois lados da comunicação... não está trafegando os dados da sua aplicação num servidor
+ *        ddns terceiro... talvez seja o mais ideal para casos como testar um MVP, dá um mínimo de segurança;
+ *      - Grande parte dos códigos de servidores ddns q encontrei, ex.: https://github.com/dstapp/docker-ddns,
+ *        o servidor ddns apenas responde ao client qual é o ip dinâmico, e o client faz uma nova request para
+ *        o ip dinâmico (ou o server ddns faz um proxy para esse ip dinâmico). Contudo, pode ser o caso do seu
+ *        provedor local estar usando a técnica CGNAT.
+ */
+
 // TODO: Mover isso para "exemples"
 const PORT = 3000;
 const TOKEN = "my_super_token";
